6e91dbce1a6757521e48ce8389e81dbf3c74ac90
范凯旋 <kaixuan.fan@pactera.com>
2018-01-11 18:04:43 +0800

    angular

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,6 +1,8 @@
 var app=angular.module('app',[])
 
 app.controller('xcCtrl',['$scope','$http',function(s,$http){
+	s.uploadBaseUrl="http://tp.taodama.net/mobile/photo/"
+	s.baseUrl='http://tp.taodama.net/'
 	//储存类型数据
 	s.tempData={
 		//背景
@@ -49,9 +51,29 @@ app.controller('xcCtrl',['$scope','$http',function(s,$http){
 		for( var i=0;i<s.itemList.length;i++ ){
 			if(s.itemList[i].id==box.id ){
 				s.currentItem=s.itemList[i]
+				s.currentItem.border="1px dashed #f00"
+			}else{
+				s.itemList[i].border="none"
 			}
 		}
+
 	}
+
+	//deleteItems
+	//删除元素
+	s.deleteItems=function(){
+		if( s.itemList.length<1 ) return
+		if( !s.currentItem.id ) return
+		var index=null;
+		for(var i=0;i<s.itemList.length;i++){
+			if( s.currentItem.id===s.itemList[i].id ){
+				index=i;
+			}
+		}
+		s.itemList.splice(index,1)
+	}
+
+
 	//背景图片默认大小
 	s.canvasComputed={
 		width:960,
@@ -99,7 +121,14 @@ app.controller('xcCtrl',['$scope','$http',function(s,$http){
 		let current=e.currentTarget
 		s.tempPage.x=pageX-current.offsetLeft
 		s.tempPage.y=pageY-current.offsetTop
-		s.currentItem=model
+		for( var i=0;i<s.itemList.length;i++ ){
+			if(s.itemList[i].id==model.id ){
+				s.currentItem=model
+				s.currentItem.border="1px dashed #f00"
+			}else{
+				s.itemList[i].border="none"
+			}
+		}
 		s.isMove=true
 	}
 
@@ -116,21 +145,6 @@ app.controller('xcCtrl',['$scope','$http',function(s,$http){
 		var fanliyX=pageX-s.tempPage.x
 		model.left=fanliyX
 		model.top=fanliyY
-		// return;
-		// if(  fanliyX>0 &&  pageX-s.tempPage.x<cvsWrap.outerWidth()-cur.clientWidth ){
-		// 	model.left=fanliyX
-		// }else if( cur.offsetLeft+cur.clientWidth >= cvsWrap.outerWidth() ){
-		// 	model.left=cvsWrap.outerWidth()-cur.clientWidth
-		// }else if(  fanliyX<=0 ){
-		// 	model.left=0
-		// }
-		// if( fanliyY>0 && fanliyY< cvsWrap.outerHeight()-cur.clientHeight){
-		// 	model.top=fanliyY
-		// }else if( cur.offsetTop+cur.clientHeight >= cvsWrap.outerHeight() ){
-		// 	model.top=cvsWrap.outerHeight()-cur.clientHeight
-		// }else if( fanliyY<=0 ){
-		// 	model.top=0
-		// }
 	}
 
 	//关闭移动开关
@@ -153,11 +167,75 @@ app.controller('xcCtrl',['$scope','$http',function(s,$http){
 	//loading
 	s.isloading=false
 
+	//previewimage
+	//修改图片路径后预览
+	$('#upload-background').on('change',function(){
+		var file=$(this)[0].files
+		var preview=$('#preview-bg')
+		var reader  = new FileReader();
+		reader.addEventListener("load", function () {
+			preview.attr('src',reader.result)
+	  	}, false);
+
+		if (file[0]) {
+   			reader.readAsDataURL(file[0]);
+  		}
+	})
+
+	//打开上传选择主题模态
+	s.listTypeName=''
+	s.uploadPreviewModal=function(){
+		var file=$('#upload-background')[0].files
+
+		if(file.length<1) return
+		
+		if ( !(/\.(jpe?g|png)$/i.test(file[0].name)) ) return
+
+		if( file[0].name==s.UPLOADNAME ) {
+			alert('这张图片已上传')
+			return;
+		};
+		var uploadPreview=$('#upload-preview');
+		var file= $('#upload-background')[0];
+		
+		var imgBg=getFileUrl(file)
+		uploadPreview.attr('src',imgBg)
+		s.isloading=true
+		$http.get(s.uploadBaseUrl+'typelist').then(function(res){
+			s.isloading=false
+			s.typeList=res.data
+			$('#chioseType').modal()
+		})	
+	}
+
+	
+	//获取input files 对象的bolob路径
+	function getFileUrl(sourceId) { 
+		var url; 
+		if (navigator.userAgent.indexOf("MSIE")>=1) { // IE 
+			url = sourceId.value; 
+		} else if(navigator.userAgent.indexOf("Firefox")>0) { // Firefox 
+			url = window.URL.createObjectURL(sourceId.files[0]); 
+		} else if(navigator.userAgent.indexOf("Chrome")>0) { // Chrome 
+			url = window.URL.createObjectURL(sourceId.files[0]); 
+		} 
+		return url; 
+	} 
+
+	//主题名称
+	s.diyType=""
+	//背景图片名称
+	s.bgItemName=""
+	
 	//上传背景图片
 	s.uploadAdmin=function(){
 		var file=$('#upload-background')[0].files
+
 		if(file.length<1) return
-		if( file.name==s.UPLOADNAME ) {
+		
+		if ( !(/\.(jpe?g|png)$/i.test(file[0].name)) ) return
+
+		if( file[0].name==s.UPLOADNAME ) {
 			alert('这张图片已上传')
 			return;
 		};
@@ -165,18 +243,21 @@ app.controller('xcCtrl',['$scope','$http',function(s,$http){
 		let uploadFile=new FormData()
 		uploadFile.append('file',file[0])
 		uploadFile.append('ref','admin')
-		uploadFile.append('title','相册1')
+		uploadFile.append('title',s.bgItemName)
+		uploadFile.append('theme',s.diyType)
+
 		s.isloading=true
 		$.ajax({
-		  url: "http://tp.taodama.net/mobile/photo/upload",
+		  url: s.uploadBaseUrl+"upload",
 		  type: "POST",
 		  data: uploadFile,
 		  processData: false,  // 不处理数据
 		  contentType: false   // 不设置内容类型
 		}).done(function(res){
 			s.isloading=false
-			s.UPLOADNAME=file.name
+			s.UPLOADNAME=file[0].name
 			s.$apply()
+			$('#chioseType').modal('hide')
 		});
 	}
 	
@@ -184,16 +265,128 @@ app.controller('xcCtrl',['$scope','$http',function(s,$http){
 	//获取类型列表
 	s.getImgBgList=function(){
 		s.isloading=true
-		$http.get('http://tp.taodama.net/mobile/photo/typelist').then(function(res){
+		$http.get(s.uploadBaseUrl+'typelist').then(function(res){
 			s.isloading=false
 			s.typeList=res.data
 			$('#getImgList').modal()
-		})		
+		})
 	}
 
 	s.listActive=null
+	s.bgGroundList=[]
 	s.getListItem=function(model,index){
-		console.log(model,index)
 		s.listActive=index
+		s.diyType=model.theme
+		url=s.uploadBaseUrl+'photolist?theme='+s.diyType
+		$http.get(url).then(function(res){
+			if(res.status==200){
+				s.bgGroundList=res.data
+			}
+		})
+	}
+
+	s.backgroundImg=null
+	s.getBGitems=function(item){
+		console.log(  s.baseUrl+item.pic )
+		s.backgroundImg=s.baseUrl+item.pic
+		var bgimage=new Image()
+		bgimage.src=s.backgroundImg
+		bgimage.onload=function(){
+			if( bgimage.width>960 ){
+				var cw=bgimage.width/960
+				s.canvasComputed.width=bgimage.width/cw
+				s.canvasComputed.height=parseInt(bgimage.height/cw)
+			}else{
+				s.canvasComputed.width=bgimage.width
+				s.canvasComputed.height=bgimage.height
+			}
+			s.cvsRatio.rotate=(s.canvasComputed.width/s.canvasComputed.height).toFixed(3)
+			s.$apply()
+			$('#getImgList').modal('hide')
+		}
+	}
+
+	var bgimage=new Image()
+	bgimage.src='assets/imgs/items.png'
+	s.backgroundImg='assets/imgs/items.png'
+	bgimage.onload=function(){
+		s.canvasComputed.width=bgimage.width
+		s.canvasComputed.height=bgimage.height
+		s.cvsRatio.rotate=(s.canvasComputed.width/s.canvasComputed.height).toFixed(3)
+		s.$apply()
+	}
+
+	//预览已做好的一张主题
+	//preview-modal
+	s.toPreviewModal=function(){
+		$('#preview-modal').modal()
+
+
+	}
+
+
+	// //储存类型数据
+	// s.tempData={
+	// 	//背景
+	// 	bgImg:"",
+	// 	//列表单个ID
+	// 	id:"img_list_0",
+	// 	//主视图比例
+	// 	cvsRatio:10/7,
+	// 	//文字数据
+	// 	textList:[],
+	// 	//相片元素数据
+	// 	//除了opcity,rotate为实数，其他都为百分比
+	// 	list:[{
+	// 		width:0,
+	// 		height:0,
+	// 		top:0,
+	// 		left:0,
+	// 		rotate:0,
+	// 		id:'222',
+	// 		opcity:1,
+	// 		pic:"",
+	// 		aspectRatio:1.777,
+	// 	}]
+	// }
+
+	// //当前加号集合
+	// s.itemList=[]
+
+	//计算list矩阵百分比数据
+
+	function computedPercentData(list){
+		var pw=s.canvasComputed.width
+		var ph=s.canvasComputed.height
+		for(var i=0;i<list.length;i++){
+			list[i].width=(list[i].width/pw*100).toFixed(2)
+			list[i].height=(list[i].height/pw*100).toFixed(2)
+			list[i].top=(list[i].top/ph*100).toFixed(2)
+			list[i].left=(list[i].left/ph*100).toFixed(2)
+		}
+		return list
+	}
+
+	//设置最主题终数据
+	s.finallyData=[]
+	s.setFinallyData=function(){
+		var id=new Date().getTime().toString(16)
+		var list=angular.copy(s.itemList)
+		var bgImg=s.backgroundImg
+		var cvsRatio=s.cvsRatio.rotate
+		var textList=[]
+		let tempData={}
+		var clist=computedPercentData(list)
+		tempData.id=id
+		tempData.list=clist
+		tempData.bgImg=bgImg
+		tempData.cvsRatio=cvsRatio
+		tempData.textList=textList
+		console.log( tempData )
+	}
+	//生成主题
+	s.finallyPush=function(){
+		var id=new Date().getTime().toString(16)
+		console.log(id)
 	}
 }])
